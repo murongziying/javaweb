dc1fa6480c44d0c7221c1e2f01af44d6706fef57
linuxc <17621726396@163.com>
2019-02-21 22:15:48 +0800

    new change

---
+++ src/test/Daffodil_number.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.math.*;
+/*
+ * 日期：2019/2/21
+ * 作者：刘超
+ * 功能：计算水仙花数
+ * */
+
+public class Daffodil_number {
+    public static void main(String[] args) {
+        for (int i = 100; i < 999; i++) {
+            int bai = i / 100 % 10;
+            int shi = i / 10 % 10;
+            int ge = i % 10;
+            if (i == Math.pow(bai, 3) + Math.pow(shi, 3) + Math.pow(ge, 3)) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/test/Print_array.java
@@ -0,0 +1,13 @@
+package test;
+/*
+* 作者：刘超
+* 日期：2019/2/21
+* 功能：按给定格式打印数组
+* */
+
+public class Print_array {
+    public static void main(String[] args){
+        int [] array = {1,2,3,4,5,6};
+        
+    }
+}
